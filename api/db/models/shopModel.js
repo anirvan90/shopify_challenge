8d8f01dbde1cd78a068e7233e750341e340165bf
anirvan90 <anirvan.awatramani@gmail.com>
2018-09-11 16:45:02 -0400

    basic relationships established

--- api/db/models/shopModel.js
+++ api/db/models/shopModel.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const orderSchema = require("../models/orderModel").orderSchema;
-const productSchema = require("../models/productModel").productSchema;
+
 const shopSchema = new mongoose.Schema({
   name: {
     type: String,
--- api/db/seed.js
+++ api/db/seed.js
@@ -1,7 +1,7 @@
 const db = require("./index.js");
 const Product = require("./models/productModel");
 const Order = require("./models/orderModel");
-
+const Shop = require("./models/shopModel");
 let kit = new Product({
   name: "Test Product",
   sellPrice: 10.99
@@ -20,11 +20,39 @@ kat.save(err => {
   if (err) console.error("something went wrong here");
 });
 
-let kitKat = new Order({
+let kitkat = new Order({
   products: [kit, kat],
   totalSale: 12
 });
 
-kitKat.save(err => {
+let kitkit = new Order({
+  products: [kit, kit],
+  totalSale: 0
+});
+
+let katkat = new Order({
+  products: [kat, kat],
+  totalSale: 13
+});
+
+const katshop = new Shop({
+  name: "KatShop",
+  products: [kit, kat],
+  orders: [kitkat, katkat, kitkit]
+});
+
+kitkat.save(err => {
   if (err) console.error("Something went wrong on kitkat");
+});
+
+kitkit.save(err => {
+  if (err) console.error("Something went wrong on kitkit");
+});
+
+katkat.save(err => {
+  if (err) console.error("Something went wrong on katkat");
+});
+
+katshop.save(err => {
+  if (err) console.error("Something went wrong on katshop");
 });
