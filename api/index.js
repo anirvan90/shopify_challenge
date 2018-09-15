2c73dfdcf36d546c0af186a89416a38915dd1ff5
anirvan90 <anirvan.awatramani@gmail.com>
2018-09-15 09:15:28 -0400

    general clean up

--- api/index.js
+++ api/index.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
-const productRouter = require("./routers/productRouter");
-const orderRouter = require("./routers/orderRouter");
-const shopRouter = require("./routers/shopRouter");
+const productRouter = require(path.join(__dirname, "./routers/productRouter"));
+const orderRouter = require(path.join(__dirname, "./routers/orderRouter"));
+const shopRouter = require(path.join(__dirname, "./routers/shopRouter"));
 
 // Declare Port to serve application
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-// Middleware
+// MIDDLEWARE
 app.use(helmet());
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 // Routers - Authentication Logic
 
@@ -21,6 +23,11 @@ app.use(bodyParser.json());
 app.use("/api", productRouter);
 app.use("/api", orderRouter);
 app.use("/api", shopRouter);
+
+// Redirect Unimplmented Routes
+app.use((req, res) => {
+  res.status(400).json({ message: "Not Implemented" });
+});
 
 // Serve Application
 app.listen(PORT, () => {
