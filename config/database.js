a9c6ba829d38b637fa8c5cbcca8614acb692cb39
anirvan90 <anirvan.awatramani@gmail.com>
2018-09-21 13:06:56 -0400

    no real change

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 // URL For Local
-mongoose.connect("mongodb://localhost:27017/fluffykins");
+// mongoose.connect("mongodb://localhost:27017/fluffykins");
 
 // URL For Docker
-// mongoose.connect("mongodb://mongo:27017/fluffykins");
+mongoose.connect("mongodb://mongo:27017/fluffykins");
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB Connection Error:.."));
